2b19e3bef9543f6fc62695752bc43a06cb0c33e6
Reza Ebrahimpour <r.ebrahimpour93@gmail.com>
2020-08-25 14:52:37 +0430

    Rename StationTestService to StationServiceTest

--- src/test/java/com/github/rezaep/evcsms/station/service/StationServiceTest.java
+++ src/test/java/com/github/rezaep/evcsms/station/service/StationServiceTest.java
@@ -21,7 +21,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class StationTestService {
+public class StationServiceTest {
     @Mock
     private StationRepository repository;
     @Mock
